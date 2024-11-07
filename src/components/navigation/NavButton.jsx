6d43c846624866bbd8822b9ccf378109174800ed
abhi114 <nabhi531@gmail.com>
2024-11-08 00:41:20 +0530

    rotation complete

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -35,13 +35,19 @@ const getIcon = (icon)=>{
 const NavButton = ({ x, y, label, link, icon, newTab }) => {
   // backdrop-blur-[6px]: Applies a backdrop blur effect with a 6px radius, creating a frosted glass effect behind the element.
   //group: This class marks the parent <div> as a group. Any child elements can now respond to the parent’s hover state.
+  //The peer and peer-hover classes in Tailwind CSS are used to create dependencies between sibling elements, enabling styles to be applied to one element based on the state of another.
+  //top-0, left-0, w-full, h-full: Positions the element to cover the full area of its parent.
+  //whitespace-nowrap: Prevents the text from wrapping onto multiple lines.
   return (
-    <div className='absolute group cursor-pointer z-50' style={{ transform: `translate(${x}, ${y})` }}>
+    <div className='absolute cursor-pointer z-50' style={{ transform: `translate(${x}, ${y})` }}>
       <Link href={link} className='text-foreground rounded-full flex items-center justify-center bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm' aria-label={label} target={newTab?'_blank':'_self'} name={label}>
       
-      <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause group-hover:text-accent'>
+      <span className='relative  w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent'>
         {getIcon(icon)}
+        <span className='peer bg-transparent absolute top-0 left-0 w-full h-full'/>
+        <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap'>{label}</span>
       </span>
+      
       </Link>
     </div>
   );
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -8,7 +8,7 @@ const Navigation = () => {
 
   return (
     <div className='w-full fixed h-screen flex items-center justify-center'>
-        <div className='flex items-center justify-center hover:pause relative animate-spin-slow'>
+        <div className='flex items-center justify-center hover:pause relative animate-spin-slow group'>
         {BtnList.map((btn,index)=>{
             //formulae to convert angle to radian
             const angleRad = (index*angleIncrement*Math.PI)/180
