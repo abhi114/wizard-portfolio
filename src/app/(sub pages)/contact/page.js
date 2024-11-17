9ea1a6162405cd682a8ca30e0887503676488b8d
abhi114 <nabhi531@gmail.com>
2024-11-17 20:14:18 +0530

    responsive completed

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -14,14 +14,14 @@ export default function Contact() {
       <Image
         src={bg}
         alt="background-image"
-        className="w-full h-full top-0 left-0 fixed object-cover object-center opacity-20"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
       />
-      <article className="relative w-full flex flex-col items-center justify-center space-y-8">
-        <div className="flex flex-col items-center justify-center space-y-6 w-3/4">
+      <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
             Summon The Wizard
           </h1>
-          <p className="text-center font-light">
+          <p className="text-center font-light text-sm xs:text-base">
             Step into the circle of enchantment and weave your words into the
             fabric of the cosmos. Whether you seek to conjure collaborations,
             unlock mysteries, or simply share tales of adventure, your messages
