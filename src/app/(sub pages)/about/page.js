39d76294ff970956a6437859b70e55d7b8587492
abhi114 <nabhi531@gmail.com>
2024-11-17 19:44:54 +0530

    about page responsive

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -16,15 +16,15 @@ export default function Home() {
         className="w-full h-full top-0 left-0 fixed object-cover object-center opacity-20"
       />
 
-      <div className="w-full h-screen absolute top-1/2 -translate-y-1/2 left-0 ">
+      <div className="w-full h-1/2 xs:h-3/4 sm:h-screen absolute top-1/2 -translate-y-1/2 left-0 ">
         <RenderModal>
           <HatModel />
         </RenderModal>
       </div>
       <div className="relative w-full h-screen flex flex-col items-center justify-center ">
-        <div className="absolute flex flex-col items-center text-center top-[65%] left-1/2 -translate-y-1/2 -translate-x-1/2">
-          <h1 className="font-bold text-7xl text-accent p-3 text-nowrap">Abhishek Nigam</h1>
-          <p className="font-light text-foreground text-ls">Meet the Wizard Behind this Portfolio</p>
+        <div className="absolute flex flex-col items-center text-center top-[55%] sm:top-[60%] lg:top-[64%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+          <h1 className="font-bold text-3xl xs:text-6xl sm:text-7xl lg:text-7xl text-accent text-nowrap">Abhishek Nigam</h1>
+          <p className="font-light text-foreground pt-2 text-ls">Meet the Wizard Behind this Portfolio</p>
         </div>
       </div>
       <AboutDetails/>
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const ItemLayout = ({children,className})=>{
   return (
-    <div className={clsx('custom-bg p-8 rounded-xl flex items-center justify-center space-y-8',className)}>
+    <div className={clsx('custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-8',className)}>
                 {children}
             </div>
   )
@@ -12,32 +12,32 @@ const ItemLayout = ({children,className})=>{
  const AboutDetails = () => {
   return (
     <section className='py-20 w-full'>
-        <div className='grid grid-cols-12 gap-8 w-full'>
-            <ItemLayout className={'col-span-8 row-span-2 flex flex-col items-start'}>
-                <h2 className='text-xl text-left w-full capitalize'>
+        <div className='grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full'>
+            <ItemLayout className={'col-span-full lg:col-span-8 row-span-2 flex flex-col items-start'}>
+                <h2 className='text-xl md:text-2xl text-left w-full capitalize'>
                     Architect of Enchantment
                 </h2>
-                <p className='font-light'>
+                <p className='font-light text-xs sm:text-sm md:text-base'>
                     Fueled by a passion for development, I bring ideas to life with a powerful mix of tools and languages. JavaScript sits at the core of my work, guiding every project I undertake. With frameworks like React.js and Next.js, I craft seamless, user-centric websites that feel intuitive and immersive.
 In mobile development, I specialize in creating responsive Android apps using React Native and Java, delivering smooth and reliable experiences across devices. On the backend, I leverage Express to build robust and scalable APIs, ensuring seamless data flow and performance. With a keen eye for UI, I ensure every project is not only functional but also visually compelling, putting user experience at the forefront. I’m always eager to explore new technologies and push the boundaries of what’s possible.
                 </p>
             </ItemLayout>
-             <ItemLayout className={'col-span-4 text-accent'}>
-            <p className='font-semibold w-full text-left text-5xl'>
+             <ItemLayout className={' col-span-full xs:col-span-6 lg:col-span-4 text-accent'}>
+            <p className='font-semibold w-full text-left text-2xl sm:text-5xl'>
                 25 + <sub className='font-semibold text-base'>Projects</sub>
             </p>
             </ItemLayout>
-             <ItemLayout className={'col-span-4 text-accent'}>
-                <p className='font-semibold w-full text-left text-5xl'>
+             <ItemLayout className={'col-span-full xs:col-span-6 lg:col-span-4 text-accent'}>
+                <p className='font-semibold w-full text-left text-2xl sm:text-5xl'>
                 2 + <sub className='font-semibold text-base'>Years of Experience</sub>
             </p>
             </ItemLayout>
 
-            <ItemLayout className={"col-span-4 !p-0"}>
+            <ItemLayout className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}>
                 <img className='w-full h-auto' src='https://github-readme-stats.vercel.app/api/top-langs/?username=abhi114&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false' alt='Abhishek' loading='lazy'/>
             </ItemLayout>
 
-            <ItemLayout className={"col-span-8 !p-0"}>
+            <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
                 <img className='w-full h-auto' src='https://github-readme-stats.vercel.app/api?username=abhi114&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false' alt='Abhishek' loading='lazy'/>
             </ItemLayout>
 
@@ -45,10 +45,10 @@ In mobile development, I specialize in creating responsive Android apps using Re
                 <img className='w-full h-auto' src='https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn' alt='Abhishek' loading='lazy'/>
             </ItemLayout>
             
-            <ItemLayout className={"col-span-6 !p-0"}>
+            <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
                 <img className='w-full h-auto' src="https://github-readme-streak-stats.herokuapp.com?user=abhi114&theme=dark&hide_border=true&type=png&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B" alt="GitHub Streak" loading='lazy'/>
             </ItemLayout>
-            <ItemLayout className={"col-span-6 !p-0"}>
+            <ItemLayout className={"col-span-full  md:col-span-6 !p-0"}>
                 <Link className=' w-full h-auto' href={'https://github.com/abhi114/ai-calculator'} target={"_blank"}>
                 <img className='w-full h-auto' src='https://github-readme-stats.vercel.app/api/pin/?username=abhi114&repo=ai-calculator&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2' alt='Abhishek' loading='lazy'/>
                 </Link>
