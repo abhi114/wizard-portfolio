a303316a77a3fa03ef97e321c694d7585534ced8
abhi114 <nabhi531@gmail.com>
2024-11-17 16:58:12 +0530

    responsive added

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -1,6 +1,7 @@
 import { Github, Home, Linkedin, Notebook, Palette, Phone, Twitter, User } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
+import ResponsiveComponent from '../ResponsiveComponent';
 const getIcon = (icon)=>{
   switch (icon) {
     case 'home':
@@ -39,7 +40,9 @@ const NavButton = ({ x, y, label, link, icon, newTab }) => {
   //top-0, left-0, w-full, h-full: Positions the element to cover the full area of its parent.
   //whitespace-nowrap: Prevents the text from wrapping onto multiple lines.
   return (
-    <div className='absolute cursor-pointer z-50' style={{ transform: `translate(${x}, ${y})` }}>
+    <ResponsiveComponent>
+      {({size})=>{
+        return size && size >=480?<div className='absolute cursor-pointer z-50' style={{ transform: `translate(${x}, ${y})` }}>
       <Link href={link} className='text-foreground rounded-full flex items-center justify-center custom-bg' aria-label={label} target={newTab?'_blank':'_self'} name={label}>
       
       <span className='relative  w-14 h-14 p-4 animate-spin-slow-reverse group-hover:pause hover:text-accent'>
@@ -49,7 +52,21 @@ const NavButton = ({ x, y, label, link, icon, newTab }) => {
       </span>
       
       </Link>
+    </div>:(
+      <div className=' cursor-pointer z-50' >
+      <Link href={link} className='text-foreground rounded-full flex items-center justify-center custom-bg' aria-label={label} target={newTab?'_blank':'_self'} name={label}>
+      
+      <span className='relative w-10 h-10 xs:w-14 xs:h-14 p-2.5 xs:p-4 hover:text-accent'>
+        {getIcon(icon)}
+        <span className='peer bg-transparent absolute top-0 left-0 w-full h-full'/>
+        <span className='absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap'>{label}</span>
+      </span>
+      
+      </Link>
     </div>
+    )
+      }}
+    </ResponsiveComponent>
   );
 };
 
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -2,18 +2,26 @@
 import { BtnList } from '@/app/data'
 import React from 'react'
 import NavButton from './NavButton';
+import useScreenSize from '../hooks/useScreenSize';
+import ResponsiveComponent from '../ResponsiveComponent';
 
 const Navigation = () => {
     const angleIncrement = 360/BtnList.length;
-
+    const size = useScreenSize();
+    console.log(size);
+    const isLarge = size >=1024;
+    const isMedium = size >=768;
   return (
     <div className='w-full fixed h-screen flex items-center justify-center'>
-        <div className='flex items-center justify-center hover:pause relative animate-spin-slow group'>
+       <ResponsiveComponent>
+        {({size})=>{
+          return size && size>=480?
+           <div className='flex items-center justify-center hover:pause relative animate-spin-slow group'>
         {BtnList.map((btn,index)=>{
             //formulae to convert angle to radian
             const angleRad = (index*angleIncrement*Math.PI)/180
-            console.log(angleRad)
-            const radius = 'calc(21vw - 1rem)'
+            //console.log(angleRad)
+            const radius = isLarge ?'calc(21vw - 1rem)':isMedium ? 'calc(30vw - 1rem)' : 'calc(40vw - 1rem)'
             const x = `calc(${radius}*${Math.cos(angleRad)})`
             const y = `calc(${radius}*${Math.sin(angleRad)})`
             return (
@@ -21,7 +29,32 @@ const Navigation = () => {
                 
             )
         })}
+        </div>:(
+          <>
+          <div className='w-full px-2.5 xs:p-0 xs:w-max flex flex-col space-y-4 items-start xs:items-center justify-center  relative  group'>
+        {BtnList.slice(0,BtnList.length/2).map((btn,index)=>{
+            //formulae to convert angle to radian
+           
+            return (
+                <NavButton key={btn.label} x={0} y={0} {...btn}/>
+                
+            )
+        })}
         </div>
+        <div className='w-full px-2.5 xs:p-0 xs:w-max flex flex-col space-y-4 items-end xs:items-center justify-center  relative  group'>
+        {BtnList.slice(BtnList.length/2,BtnList.length).map((btn,index)=>{
+            //formulae to convert angle to radian
+           
+            return (
+                <NavButton key={btn.label} x={0} y={0} {...btn}/>
+                
+            )
+        })}
+        </div>
+          </>
+        )
+        }}
+       </ResponsiveComponent>
     </div>
   )
 }
