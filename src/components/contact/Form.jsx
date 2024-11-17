94a56254f00ddaeace1294110194ee9536435b7a
abhi114 <nabhi531@gmail.com>
2024-11-18 02:34:28 +0530

    animations added

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -3,6 +3,21 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from "@emailjs/browser";
 import { toast, Toaster } from 'sonner';
+import { motion } from 'framer-motion';
+const item = {
+  hidden:{scale:0},
+  show:{scale:1},
+}
+const container = {
+  hidden:{opacity:0},
+  show:{
+    opacity:1,
+    transition:{
+      staggerChildren:0.3,
+      delayChildren:0.2
+    }
+  }
+}
 export default function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = (data) => {
@@ -40,14 +55,14 @@ export default function Form() {
   return (
    <>
    <Toaster theme='dark' richColors={true}/>
-    <form onSubmit={handleSubmit(onSubmit)} className='max-w-md w-full flex flex-col items-center justify-center space-y-4'>
-      <input type="text" placeholder="Name" {...register("Name", {required:'This Field is Required',minLength:{value:3,message:"Name should be atleast 3 characters long"}})} className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'/>
+    <motion.form variants={container} initial="hidden" animate="show" onSubmit={handleSubmit(onSubmit)} className='max-w-md w-full flex flex-col items-center justify-center space-y-4'>
+      <motion.input variants={item} type="text" placeholder="Name" {...register("Name", {required:'This Field is Required',minLength:{value:3,message:"Name should be atleast 3 characters long"}})} className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'/>
       {errors.Name && <span className='inline-block self-start text-accent text-sm custom-bg rounded-md p-1'>{errors.Name.message}</span>}
-      <input type="email" placeholder="email" {...register("email", {required: 'This Field is Required'})} className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'/>
+      <motion.input variants={item} type="email" placeholder="email" {...register("email", {required: 'This Field is Required'})} className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'/>
       {errors.email && <span className='inline-block self-start text-accent text-sm custom-bg rounded-md p-1'>{errors.email.message}</span>}
-      <textarea placeholder='message' {...register("message", {required: 'This Field is Required', maxLength:{value:500,message:'Message Should be less than 500 characters'}, minLength:{value:50,message:'Message Should be atleast 50 characters'}})} className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'/>
+      <motion.textarea variants={item} placeholder='message' {...register("message", {required: 'This Field is Required', maxLength:{value:500,message:'Message Should be less than 500 characters'}, minLength:{value:50,message:'Message Should be atleast 50 characters'}})} className='w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg'/>
        {errors.message && <span className='inline-block self-start text-accent text-sm custom-bg rounded-md p-1'>{errors.message.message}</span>}
-      <input value={"Cast Your Message"} className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop:blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg cursor-pointer capitalize' type="submit" />
-    </form></>
+      <motion.input variants={item} value={"Cast Your Message"} className='px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop:blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg cursor-pointer capitalize' type="submit" />
+    </motion.form></>
   );
 }
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,13 +1,24 @@
+"use client"
 import React from 'react'
 import Projectlayout from './Projectlayout'
-
+import { motion } from 'framer-motion'
+const container = {
+  hidden:{opacity:0},
+  show:{
+    opacity:1,
+    transition:{
+      staggerChildren:0.3,
+      delayChildren: 1.5,
+    }
+  }
+}
  const ProjectList = ({projects}) => {
   return (
-     <div className='w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center py-4'>
+     <motion.div variants={container} initial="hidden" animate="show" className='w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center py-4'>
         {projects.map((project,index)=>{
             return (<Projectlayout key={index} {...project}/>)
         })}
-     </div>
+     </motion.div>
   )
 }
 
