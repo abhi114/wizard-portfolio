a44bdabfa045755ad0eab480076435d00cfc2936
abhi114 <nabhi531@gmail.com>
2024-11-17 20:07:41 +0530

    projects page responsive

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -3,7 +3,7 @@ import Projectlayout from './Projectlayout'
 
  const ProjectList = ({projects}) => {
   return (
-     <div className='w-full max-w-4xl px-16 space-y-8 flex flex-col items-center py-4'>
+     <div className='w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center py-4'>
         {projects.map((project,index)=>{
             return (<Projectlayout key={index} {...project}/>)
         })}
