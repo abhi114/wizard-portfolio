1bad13b0d191de6029a59c8a0f8e96d2b2ac8656
abhi114 <nabhi531@gmail.com>
2024-11-18 18:25:43 +0530

    new music added

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -83,7 +83,7 @@ const Sound = () => {
     <div className="fixed top-4 right-2.5 xs:right-4 z-50 group">
         {showModal && <Modal onClose={()=>setShowModal(false)} toggle={toggle}/>}
         <audio ref={audioRef} loop>
-            <source src={"/audio/videoplayback.mp3"} type='audio/mpeg'/>
+            <source src={"/audio/videoplayback2.mp3"} type='audio/mpeg'/>
             your browser does not support the audio element.
         </audio>
         <motion.button onClick={toggle} initial={{scale:0}} animate={{scale:1}} transition={{delay:1}} className='w-10 h-10 xs:w-14 xs:h-14 text-foreground rounded-full flex items-center justify-center cursor-pointer z-50 p-2.5 xs:p-4 custom-bg'>
